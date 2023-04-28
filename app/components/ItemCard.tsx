8a90ee33de437f40aea4543affdf495bf66814e4
Justin Schreiber <justintschreiber@gmail.com>
2023-04-27 22:34:14 -0700

    a little polish

--- app/components/ItemCard.tsx
+++ app/components/ItemCard.tsx
@@ -26,14 +26,14 @@ export const ItemCard = ({ product }: { product: IProduct }) => {
                     <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z" clipRule="evenodd" />
                 </svg>
             </a>
-            {price.listPrice !== price.price && <div className={"absolute top-0 left-0 m-2 px-4 bg-custom-gray-200 rounded-sm"}>{ruleOf100(price)} Off</div>}
+            {price.listPrice !== price.price && <div className={"absolute top-0 left-0 m-2 px-4 bg-custom-gray-200 rounded-sm text-black"}>{ruleOf100(price)} Off</div>}
             <img
                 className="object-contain max-w-full my-auto"
                 src={image}
                 alt={title}
             />
             <div className={"flex flex-col gap-2 mt-auto"}>
-                <div className={"flex flex-row justify-between gap-2"}>
+                <div className={"flex flex-row justify-between gap-2 items-center"}>
                     <h3 className="opacity-70 text-custom-gray-100 w-100 text-sm">{title}</h3>
                     <div className="text-sm text-custom-gray-100 text-opacity-90 mt-auto flex flex-col items-end">
                         {(price.price === '0.00') ? "Free" : `$${price.price}`}
